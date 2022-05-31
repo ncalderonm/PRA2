d4b3813a8fd18a6c058dc2872c58720e0617a21e
ncalderonm <ncalderonm@uoc.edu>
2022-05-31 17:33:05 +0200

    commite

--- Answers.py
+++ Answers.py
@@ -8,10 +8,11 @@ import csv
 
 # Reading dataset
 
-"""with open('Data Titanic\gender_submission.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)"""
-
 gender_submission = pd.read_csv('Data Titanic\gender_submission.csv', header=0)
 print(gender_submission.sample(5))
+
+test_df = pd.read_csv('Data Titanic\test.csv', header=0)
+print(test_df.sample(5))
+
+train_df = pd.read_csv('Data Titanic\train.csv', header=0)
+print(train_df.sample(5))
