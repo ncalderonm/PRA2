b9a5d26992686e0476a1fb4ac6607b6e42615062
ncalderonm <ncalderonm@uoc.edu>
2022-05-31 17:36:56 +0200

    commite

--- Answers.py
+++ Answers.py
@@ -11,8 +11,8 @@ import csv
 gender_submission = pd.read_csv('Data Titanic\gender_submission.csv', header=0)
 print(gender_submission.sample(5))
 
-test_df = pd.read_csv('Data Titanic\test.csv', header=0)
+test_df = pd.read_csv("Data Titanic\df_test.csv", header=0)
 print(test_df.sample(5))
 
-train_df = pd.read_csv('Data Titanic\train.csv', header=0)
+train_df = pd.read_csv('Data Titanic\df_train.csv', header=0)
 print(train_df.sample(5))
